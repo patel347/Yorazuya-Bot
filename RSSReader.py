d3070f1d497f11aff09ef05ee2e2c1d2bb4c06b1
Unknown <thepatel347@gmail.com>
2017-08-29 19:59:14 +0100

    Changed RSSReader into a class and removed data text file

--- RSSReader.py
+++ RSSReader.py
@@ -1,82 +1,72 @@
 import feedparser
 import time
 
-RSS_LINK = 'http://euw.leagueoflegends.com/en/rss.xml'
-DATA_FILE = 'RSSData.txt'
+class RSSReader:
+    """Class built upon feedparser to get new items from an rss feed"""
+    DATA_FILE = 'RSSData.txt'
+
+    def __init__(self, rssLink):
+        self.rssLink = rssLink
+
+    DATA_FILE = 'RSSData.txt'
+
+    def getDateOfLatestRead(self):
+        dateOfLatestRead = None
+
+        try:
+            dataFile  = open(RSSReader.DATA_FILE,'r')
+            dateOfLatestRead = dataFile.readline()
+            dataFile.close()
+        except FileNotFoundError:
+            print("file not found, making file")
+            dateOfLatestRead = 'Sat, 05 Aug 2017 19:34:59 +0000' #date this file was created
+            dataFile  = open(RSSReader.DATA_FILE,'w+')
+            dataFile.write(dateOfLatestRead)
+            dataFile.close()
+
+        dateOfLatestRead  = time.strptime(dateOfLatestRead, "%a, %d %b %Y %H:%M:%S %z") 
+
+        return dateOfLatestRead
 
 
-def main():
-
-    feed = feedparser.parse(RSS_LINK)
-
-    # latestItemDate = feed.entries[0].published_parsed
-
-   
-
-    
-    latestDateRead = getDateOfLatestRead()
-
-    newItems = getNewItems(latestDateRead)
-    if newItems != None:
-        for item in newItems:
-            print(item.title.encode('utf8'))
-            print('\n')
-
-    input()
-
-def getDateOfLatestRead():
-    dateOfLatestRead = None
-
-    try:
-        dataFile  = open(DATA_FILE,'r')
-        dateOfLatestRead = dataFile.readline()
-        dataFile.close()
-    except FileNotFoundError:
-        print("file not found, making file")
-        dateOfLatestRead = 'Sat, 05 Aug 2017 19:34:59 +0000' #date this file was created
-        dataFile  = open(DATA_FILE,'w+')
-        dataFile.write(dateOfLatestRead)
+    def setDateOfLatestRead(self,dateToSet):
+        dataFile  = open(RSSReader.DATA_FILE,'w+')
+        dataFile.write(dateToSet)
         dataFile.close()
 
-    dateOfLatestRead  = time.strptime(dateOfLatestRead, "%a, %d %b %Y %H:%M:%S %z") 
 
-    return dateOfLatestRead
+    def getNewItems(self,latestDateRead):
+      
 
+        feed = feedparser.parse(self.rssLink)
+        newLatestDateReadParsed = feed.entries[0].published_parsed
+        newLatestDateRead =  feed.entries[0].published
 
-def setDateOfLatestRead(dateToSet):
-    dataFile  = open(DATA_FILE,'w+')
-    dataFile.write(dateToSet)
-    dataFile.close()
+        newItems= []
 
+        for item in feed.entries:
+            #manual parsing because saved date has been manually parsed and has a -1
+            #the is_dst value casuing the comparisons to be incorrect.
+            item.parsedDate  = time.strptime(item.published, "%a, %d %b %Y %H:%M:%S %z") 
+            if latestDateRead < item.parsedDate:
+                newItems.insert(0,item)
+            if  item.parsedDate> newLatestDateReadParsed:
+                newLatestDateRead = item.published
+                newLatestDateReadParsed = item.parsedDate
 
-def getNewItems(latestDateRead):
-  
+        self.setDateOfLatestRead(newLatestDateRead)
+        return newItems
 
-    feed = feedparser.parse(RSS_LINK)
-    newLatestDateReadParsed = feed.entries[0].published_parsed
-    newLatestDateRead =  feed.entries[0].published
+    def printToConsole(self):
+        feed = feedparser.parse(RSS_LINK)
+        RSS_LINK = 'http://euw.leagueoflegends.com/en/rss.xml'
+         
+        latestDateRead = getDateOfLatestRead()
 
-    newItems= []
+        newItems = getNewItems(latestDateRead)
+        if newItems != None:
+            for item in newItems:
+                print(item.title.encode('utf8'))
+                print('\n')
 
-    for item in feed.entries:
-        #manual parsing because saved date has been manually parsed and has a -1
-        #the is_dst value casuing the comparisons to be incorrect.
-        item.parsedDate  = time.strptime(item.published, "%a, %d %b %Y %H:%M:%S %z") 
-        if latestDateRead < item.parsedDate:
-            newItems.insert(0,item)
-        if  item.parsedDate> newLatestDateReadParsed:
-            newLatestDateRead = item.published
-            newLatestDateReadParsed = item.parsedDate
-
-
-    setDateOfLatestRead(newLatestDateRead)
-    return newItems
-
-# def setRSSFeedLink():
-
-# def printToConsole():
-
-if __name__ == "__main__":
-    main()
-
-
+        input()
