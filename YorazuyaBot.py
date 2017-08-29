28e6c127104f0333807b818d03073c11afd1e145
Unknown <thepatel347@gmail.com>
2017-08-29 20:00:12 +0100

    Bot Now prints latest news stories from LOL RSS feed

--- YorazuyaBot.py
+++ YorazuyaBot.py
@@ -1,19 +1,26 @@
 import requests
+from RSSReader import RSSReader
 channelID = 147031379438338048
+NEWS_CHANNEL_ID = 156859545916801024
 
-def main():
-    # my code here
-    #helloString = "Hello World!"
-    userinput = raw_input("Enter a message.")
+def sendMessage(channelID,message):
     token = "Bot MjQ5MjcwMzU3MTY3NDM5ODcz.CxEPDg.rca_6ll3s2RhqoeqJZQonIzhJdU"
     Authorization ={'Authorization': token}
-    r = requests.post('https://discordapp.com/api/channels/%s/messages' %channelID, data = {'content': userinput}, headers = Authorization)
-    print userinput
-    print r.text
+    r = requests.post('https://discordapp.com/api/channels/%s/messages' %channelID, data = {'content': message}, headers = Authorization)
 
+def getLatestLeagueNews():
+    RSS_LINK = 'http://euw.leagueoflegends.com/en/rss.xml'
+    LeagueNewsReader = RSSReader(RSS_LINK)
+    latestDateRead = LeagueNewsReader.getDateOfLatestRead()
+    newItems = LeagueNewsReader.getNewItems(latestDateRead)
+    if newItems != None:
+        for item in newItems:
+            sendMessage(NEWS_CHANNEL_ID,item.title + ' ' + item.link )
+
+
+def main():
+    getLatestLeagueNews()
 
 
 if __name__ == "__main__":
     main()
-
-
